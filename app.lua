f0725ba155840492c035caeb483696abd25ee304
Boris Nagaev <bnagaev@gmail.com>
2014-10-28 15:24:27 +0300

    before_filter for translation

--- app.lua
+++ app.lua
@@ -1,12 +1,23 @@
 local lapis = require("lapis")
 local csrf = require("lapis.csrf")
+local load_mo_file = require("load_mo_file")
 
 local fbb = require("fbb")
 local preps = require("preps")
 
 local app = lapis.Application()
 
-local _ = function(t) return t end
+local tr_ru = assert(load_mo_file(nil))
+
+-- FIXME https://github.com/leafo/lapis/issues/188
+app.__class:before_filter(function(self)
+    local lang = self.req.headers['Accept-Language']:sub(1, 2)
+    if lang == 'ru' then
+        self._ = tr_ru
+    else
+        self._ = function(t) return t end
+    end
+end)
 
 local check_user = function(f)
     return function(self)
@@ -21,8 +32,8 @@ end
 local check_prep = function(f)
     return check_user(function(self)
         if preps.as_dict[self.session.user] == nil then
-            self.title = _("Permission denied")
-            return _("Only preps can see this page")
+            self.title = self._("Permission denied")
+            return self._("Only preps can see this page")
         else
             return f(self)
         end
@@ -32,8 +43,8 @@ end
 local check_csrf = function(f)
     return function(self)
         if not csrf.validate_token(self) then
-            self.title = _("Permission denied")
-            return _("Bad csrf token")
+            self.title = self._("Permission denied")
+            return self._("Bad csrf token")
         else
             return f(self)
         end
