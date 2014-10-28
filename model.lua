ca31197ed7b0c7e5cec3cc1f0f2356d3b2be5420
Boris Nagaev <bnagaev@gmail.com>
2014-10-28 18:41:38 +0300

    model.new_quiz

--- model.lua
+++ model.lua
@@ -1,6 +1,8 @@
 local schema = require("lapis.db.schema")
 local Model = require("lapis.db.model").Model
 
+local quizs = require('quiz.all')
+
 local model = {}
 
 function model.create_schema()
@@ -33,6 +35,26 @@ end
 
 model.Task = Model:extend("task")
 model.Quiz = Model:extend("quiz", {timestamp=true})
+
+function model.new_quiz(app)
+    local user = app.session.user
+    local quiz_name = app.req.params_post.name
+    local q = quizs[quiz_name]
+    if not q then
+        error(app._("No such quiz found"))
+    end
+    local quiz = model.Quiz:create({name=quiz_name, user=user,
+        right_answers=0, finished=false})
+    for task_name, func in pairs(q) do
+        if task_name ~= 'task' then
+            local text, a1, a2, a3, a4 = func(app)
+            model.Task:create({quiz_id=quiz.id, name=task_name,
+                text=text, a1=a1, a2=a2, a3=a3, a4=a4,
+                selected=0})
+        end
+    end
+    return quiz
+end
 
 return model
 
