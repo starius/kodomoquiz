5fb71e3788ea338b19dab7caf2330fb71d56688b
Boris Nagaev <bnagaev@gmail.com>
2014-10-28 16:26:59 +0300

    add kodomo.lua
    common file paths on kodomo

---
+++ kodomo.lua
@@ -0,0 +1,15 @@
+local KODOMO = 'kodomo.fbb.msu.ru'
+
+local kodomo = {}
+
+function kodomo.url_of(user)
+    return 'http://' .. KODOMO .. '/~' .. user .. '/'
+end
+
+function kodomo.a_of(u)
+    return '<a href="' .. url_of(u) ..
+        '" target="_blank">' .. u ..'</a>'
+end
+
+return kodomo
+
