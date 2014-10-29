b07fa69e031fa44f0bca8c97e2583ba09b1f7027
Boris Nagaev <bnagaev@gmail.com>
2014-10-29 04:42:43 +0300

    load .mo file if it exists

--- tr_ru.lua
+++ tr_ru.lua
@@ -1,3 +1,18 @@
 local load_mo_file = require("load_mo_file")
-return assert(load_mo_file(nil))
+
+function file_exists(name)
+    local f = io.open(name, "r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    else
+        return false
+    end
+end
+
+if file_exists("ru/messages.mo") then
+    return assert(load_mo_file("ru/messages.mo"))
+else
+    return function(t) return t end
+end
 
