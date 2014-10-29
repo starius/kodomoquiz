ccaae3058fe0beb42b8dcfbc2fcc4126c4e30b78
Boris Nagaev <bnagaev@gmail.com>
2014-10-29 03:40:03 +0300

    add tasks for division and mod

--- quiz/hello.lua
+++ quiz/hello.lua
@@ -40,5 +40,44 @@ function hello.print_sum(req)
     task(req)
 end
 
+function hello.division_int(req)
+    local a = math.random(60, 100)
+    local b = math.random(10, 50)
+    local result = math.floor(a / b)
+    return
+    f(">>> print(%i // %i)", a, b),
+    f('%i', result),
+    f('%i', result + 1),
+    f('%i', result - 1),
+    f('%i', result - 2),
+    task(req)
+end
+
+function hello.division_float(req)
+    local result = 0.5 * math.random(2, 5)
+    local b = math.random(10, 50)
+    local a = result * b
+    return
+    f(">>> print(%i.0 / %i.0)", a, b),
+    f('%0.1f', result),
+    f('%0.1f', result - 0.5),
+    f('%0.1f', result + 0.5),
+    f('%0.1f', result * 2),
+    task(req)
+end
+
+function hello.mod(req)
+    local a = math.random(60, 100)
+    local b = math.random(10, 50)
+    local result = a % b
+    return
+    f(">>> print(%i %% %i)", a, b),
+    f('%i', result),
+    f('%i', result + 1),
+    f('%i', result - 1),
+    f('%i', a),
+    task(req)
+end
+
 return hello
 
