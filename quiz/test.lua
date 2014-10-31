4fe2c3064242bae9c9d3ad6e57859c4512afba3c
Boris Nagaev <bnagaev@gmail.com>
2014-10-31 15:02:48 +0300

    test: make sure text and answers are not nil

--- quiz/test.lua
+++ quiz/test.lua
@@ -1,5 +1,6 @@
 local all = require('quiz.all')
 local all_different = require('quiz.helpers').all_different
+local has_nil = require('quiz.helpers').has_nil
 
 local N = 1000
 
@@ -9,6 +10,9 @@ for i = 1, N do
     for quiz_name, quiz in pairs(all) do
         for task_name, task in pairs(quiz) do
             local t, a1, a2, a3, a4 = task()
+            if has_nil(5, t, a1, a2, a3, a4) then
+                error(quiz_name .. '.' .. task_name)
+            end
             if not all_different(a1, a2, a3, a4) then
                 error(quiz_name .. '.' .. task_name ..
                     '\n\n' ..
