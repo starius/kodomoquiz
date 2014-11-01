4c8479c040e1eea89014837c2cbfb7bfb6707493
Boris Nagaev <bnagaev@gmail.com>
2014-11-01 20:47:00 +0300

    if_list_for_range_short

--- quiz/all.lua
+++ quiz/all.lua
@@ -2,6 +2,8 @@ local all = {}
 
 all.hello = require('quiz.hello')
 all.if_list_for_range = require('quiz.if_list_for_range')
+all.if_list_for_range_short =
+    require('quiz.if_list_for_range_short')
 
 return all
 
---
+++ quiz/if_list_for_range_short.lua
@@ -0,0 +1,32 @@
+
+local h = require('quiz.helpers')
+local orig = require('quiz.if_list_for_range')
+
+local ilfr_short = {}
+
+local group = function(...)
+    local orig_names = {...}
+    local name = orig_names[1]
+    local orig_funcs = {}
+    for _, orig_name in ipairs(orig_names) do
+        table.insert(orig_funcs, orig[orig_name])
+    end
+    assert(#orig_funcs >= 1)
+    ilfr_short[name] = h.one_of_getter(h.unpack(orig_funcs))
+end
+
+group('odd_even', 'indentation_error', 'no_indentation_error')
+group('in_str')
+group('elif', 'nocolon_error')
+group('strip')
+group('split1', 'split2')
+group('len')
+group('test_or', 'test_and')
+group('bool_expression')
+group('list_index', 'index_error', 'append', 'fake_append',
+    'negative_index')
+group('range')
+group('for2', 'test_while')
+
+return ilfr_short
+
