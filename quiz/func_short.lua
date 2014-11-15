e724304bb914c974a115c51bb76f8d2315e35884
Boris Nagaev <bnagaev@gmail.com>
2014-11-16 01:46:22 +0300

    func_short: do not group tasks of different type

--- quiz/func_short.lua
+++ quiz/func_short.lua
@@ -7,12 +7,15 @@ local func_short = {}
 local group = h.make_group(func_short, orig)
 
 group('func_summ', 'func_arg_name', 'var_undefined')
-group('sin', 'cos', 'sin_rev')
-group('argv', 'argv1', 'argv2')
-group('complement')
+group('sin', 'cos',
+group('sin_rev')
+group('argv')
+group('argv1')
+group('argv2')
 group('system')
 group('urllib2')
 group('randint')
+group('complement')
 
 return func_short
 
